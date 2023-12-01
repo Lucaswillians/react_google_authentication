46d5d19e7bab6798499a3dd8eea34f705f19997f
Lucaswillians <lucaswilianserpa@gmail.com>
2023-12-01 16:50:24 -0300

    feat::

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
 import React, { useState } from "react";
-import GoogleLogin from "react-google-login";
+import GoogleLogin from "react-google-login"; 
+ 
+function App ()
+{
+	const [name, setName] = useState(); 
+	const [email, setEmail] = useState(); 
+	const [profilePic, setProfilePic] = useState(); 
+	const [isLoggedIn, setIsLoggedIn] = useState(false); 
 
-function App() {
-	const [name, setName] = useState();
-	const [email, setEmail] = useState();
-	const [profilePic, setProfilePic] = useState();
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const responseGoogle = (response) => 
+	{ 
+		const { profileObj: { name, email, imageUrl }, 
+	 } = response; 
+		setName(name); 
+		setEmail(email);  
+		setProfilePic(imageUrl); 
+		setIsLoggedIn(true); 
+	}
 
-	const responseGoogle = (response) => {
-		console.log(response);
-		const {
-			profileObj: { name, email, imageUrl },
-		} = response;
-		setName(name);
-		setEmail(email);
-		setProfilePic(imageUrl);
-		setIsLoggedIn(true);
-	};
+
 	return (
-		<div className="container">
-			<GoogleLogin
-				clientId="366023343940-ndamikr6o5em3sivatijihspjsg8gmt2.apps.googleusercontent.com"
-				buttonText="Continuar com o Google"
-				onSuccess={responseGoogle}
-				onFailure={responseGoogle}
+    <div className="container">
+      <GoogleLogin 
+				clientId="51327336967-h5kl3utpnjlf4v6am3q399t3rfb9qagp.apps.googleusercontent.com"   
+				buttonText="Continuar com o Google" 
+				onSuccess={responseGoogle}  
+				onFailure={responseGoogle}  
 			/>
-			{isLoggedIn ? (
-				<div style={{ textAlign: "center" }}>
-					<h1>User Information</h1>
-					<img className="profile" src={profilePic} alt="Profile" />
-					<p>Name: {name}</p>
-					<p>Email: {email}</p>
-				</div>
-			) : (
-				""
-			)}
-		</div>
-	);
+			{ isLoggedIn ? <div> 
+
+			<h1>User Information</h1>  
+			<img className="profile" src={profilePic} alt="Profile"/> 
+			<p>Name: { name }</p> 
+			<p>Email: { email }</p>
+ 
+			</div> : ' '} 
+    </div> 
+  ); 
 }
 
 export default App;
